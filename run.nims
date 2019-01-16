e4af840930a5060151d75dded2c59f0dbb13fa73
SunTzu <arithmetic@protonmail.com>
2019-01-16 11:32:32 -0600

    Adding Files.

---
+++ run.nims
@@ -0,0 +1,3 @@
+exec "git clone https://github.com/Imgp3Dev/Lookup"
+exec "nim c -r Lookup"
+
---
+++ wikis.nim
@@ -0,0 +1,15 @@
+import httpclient, json, fileinput, os
+#Setting up HTTP client.
+let client = newHttpClient()
+client.headers = newHttpHeaders({"Content-Type": "application/json"})
+let body %*{
+"data": lookup
+}
+#Setting up "lookup" for the lookup function.
+let response = client.request("https://en.m.wikipedia.org/wiki/");
+if response.status == 200:
+var client = newHttpClient()
+echo client.getcontent("https://en.m.wikipedia.org/wiki/", lookup)
+var input: FileInput = CreateFileInput(@["filters"]
+proc Filter(lookup; Input)
+
