fb9fdf150c7776aaf688f1996e575c7344bffe66
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2019-01-27 14:10:37 -0600

    Update wikis.nim

--- wikis.nim
+++ wikis.nim
@@ -5,6 +5,7 @@ client.headers = newHttpHeaders({"Content-Type": "application/json"})
 let body %*{
 "data": lookup
 }
+proc Lookup*(Lookup: string) =
 #Setting up "lookup" for the lookup function.
 let response = client.request("https://en.m.wikipedia.org/wiki/");
 if response.status == 200:
